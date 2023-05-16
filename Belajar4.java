0668563f977a54f70da55af7a5bfbb1ab7071ff1
Agwexs <aryaagun04@gmail.com>
2023-05-16 08:37:57 +0700

    ini program java

---
+++ Belajar4.java
@@ -0,0 +1,9 @@
+public class Belajar4{
+
+    public static void main(String[] args) {
+        int satu = 1;
+        String nama = "Arya";
+        System.out.println(satu + nama);
+        
+    } 
+}
---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld{
+    public static void main(String[] args){
+        String nama = "Arya";
+        System.out.println("Hello World");
+        System.out.println("Halo namaku arya agung wicaksono");
+        System.out.println(30*4);
+        System.out.println("Namaku adalah " + nama);
+        System.out.println("Aku");
+
+    }
+}
